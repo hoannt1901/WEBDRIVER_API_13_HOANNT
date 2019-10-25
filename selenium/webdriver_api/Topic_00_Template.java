f6691bebea87f8c1775093f9cd79380d9d53f778
Hoan Nguyen Tien <Hoan Nguyen Tien@DESKTOP-15PCV45>
2019-10-25 23:43:01 +0700

    add class Template

---
+++ selenium/webdriver_api/Topic_00_Template.java
@@ -0,0 +1,68 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.Assert;
+
+import org.testng.annotations.AfterClass;
+
+import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.Test;
+
+
+
+public class Topic_00_Template {
+
+	// Khai báo 1 cái biến driver đại diện cho Selenium WebDriver
+
+	WebDriver driver;
+
+
+
+	// Pre-Condition
+
+	@BeforeClass
+
+	public void beforeClass() {
+		// Khởi tạo trình duyệt Firefox
+		driver = new FirefoxDriver();
+		// Chờ cho element được hiển thị trước khi tương tác trong vòng 30s
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);		
+		// Phóng to trình duyệt
+		driver.manage().window().maximize();	
+		// Mở ra 1 trang web (AUT: Application Under Test)
+		driver.get("");
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("");
+	}
+	@Test
+	public void TC_02() {	
+		driver.get("");
+	}
+	@Test
+
+	public void TC_03() {
+		driver.get("");
+	}
+	// Post-Condition
+	@AfterClass
+	public void afterClass() {
+		// Tắt trình duyệt đi
+		driver.quit();
+	}
+
+
+
+}
