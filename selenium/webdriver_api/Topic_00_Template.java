11a13e363c827a6c87d38b0925c85b242b81e1ed
Hoan Nguyen Tien <Hoan Nguyen Tien@DESKTOP-15PCV45>
2019-11-05 19:52:47 +0700

    tc01-tc04

--- selenium/webdriver_api/Topic_00_Template.java
+++ selenium/webdriver_api/Topic_00_Template.java
@@ -2,18 +2,19 @@ package webdriver_api;
 
 import java.util.concurrent.TimeUnit;
 
-
+import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.Assert;
 
 import org.testng.annotations.AfterClass;
 
 import org.testng.annotations.BeforeClass;
 
 import org.testng.annotations.Test;
-
 
 
 public class Topic_00_Template {
